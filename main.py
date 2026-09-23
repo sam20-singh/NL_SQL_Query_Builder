1f43e5b3459951c18304070b7147383fa90753ea
sam jebasingh <rsamjebasingh@gmail.com>
2026-09-23 10:55:47 +0530

    mm

--- main.py
+++ main.py
@@ -1,56 +1,39 @@
+import requests
 import sqlite3
-import request
+
+response = requests.post(
+    "http://localhost:11434/api/generate",
+    json={
+        "model": "qwen2.5:7b",
+        "prompt": """
+You are a SQL assistant.
+
+Database table:
+employees
+
+Columns:
+id - employee ID
+name - employee name
+department - employee department
+salary - employee salary
+
+Convert this request into SQL:
+Show employees whose salary is greater than 50000
+""",
+        "stream": False
+    }
+)
+
+print(response.json()["response"])
+
+
 
 connection = sqlite3.connect("database.db")
-
 cursor = connection.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS employees (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    department TEXT,
-    salary INTEGER
-)
-""")
-connection = sqlite3.connect("database.db")
+cursor.execute("SELECT * FROM employees WHERE salary > 50000")
 
-cursor = connection.cursor()
-
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS employees (
-    id INTEGER PRIMARY KEY,
-    name TEXT,
-    department TEXT,
-    salary INTEGER
-)
-""")
-
-connection.commit()
-#salary=int(input("Enter your salary :"))
-
-#cursor.execute("INSERT INTO employees (id, name, department, salary) VALUES (1, 'Arun', 'IT', 60000)")
-#cursor.execute("INSERT INTO employees (id, name, department, salary) VALUES (2, 'John', 'HR', 50000)")
-#cursor.execute("INSERT INTO employees (id, name, department, salary) VALUES (3, 'Alice', 'Finance', 70000)")
-cursor.execute("SELECT department, COUNT(*) FROM employees GROUP BY department")
-connection.commit()
-
-print("Employee added!")
-
-#cursor.execute("SELECT * FROM employees WHERE department = 'IT'")
-#cursor.execute("SELECT * FROM employees ORDER BY salary DESC LIMIT 1")
 rows = cursor.fetchall()
 
 for row in rows:
     print(row)
-#print("Employee added!")
-response = requests.post(
-    url,
-    json={"question": "Show IT employees"}
-)
-
-
-connection.commit()
-
-
-##print("Employees table created!")
